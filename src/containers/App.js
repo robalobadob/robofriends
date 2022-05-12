d16997b7c696b4bdf899141d3d58aedc2bd4e042
Rob Hoover <rtrhoover@gmail.com>
2022-05-12 12:50:43 -0700

    remove unused hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-function App() {
-    const [robots, setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    const [count, setCount] = useState(0)
+import { setSearchField } from '../actions';
 
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
     }
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {setRobots(users)});
-        console.log(count)
-    },[count])
-
-    const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ?
-        <h1>Loading</h1> :
-        (
-            <div className='tc'>
-                <h1 className="f1">RoboFriends</h1>
-                <button onClick={()=>setCount(count+1)}>Click Me!</button>
-                <SearchBox searchChange={onSearchChange} />
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        );   
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+             .then(response => response.json())
+             .then(users => this.setState({ robots: users }));
+    }
+
+    render() {
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
